c43301e96d678ab93c10a5e8317cd488ef956181
Dylan Giddens <74880352+katzMotel@users.noreply.github.com>
2024-11-10 19:43:48 -0600

    current version of ableton about page mockup, needs additional styling

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import {defineConfig} from 'vite';
+export default defineConfig({
+   build: {
+    outDir: 'dist',
+    rollupOptions:{
+        input: './index.html',
+    },
+   } ,
+   server: {
+    fs: {
+        strict: true,
+    },
+   },
+});
